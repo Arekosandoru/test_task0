be57d47792695b4a57691624ae2dc5111f3051b8
Arekosandoru <borom1993>
2016-10-29 15:14:29 +0700

    upload

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main {
         String address = "127.0.0.1";
         int port = 8746;
 
-        try  {
+        try {
             ServerSocket ss = new ServerSocket(port); // создаем сокет сервера и привязываем его к вышеуказанному порту
             System.out.println("Waiting for a client...");
             Socket socket = ss.accept(); // заставляем сервер ждать подключений
@@ -24,36 +24,39 @@ public class Main {
             System.out.println();
             // Берем входной и выходной потоки сокета, теперь можем получать и отсылать данные клиенту.
             OutputStream sout = socket.getOutputStream();
-            BufferedReader in  = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream out = new DataOutputStream(sout);
 
-            while(true) {
+            while (true) {
                 Date d = new Date();
                 Random r = new Random();
-                String result="", randch="", getword="",hexword="";
-
+                String result = "", randch = "", getword = "", hexword = "";
                 out.writeUTF(" ");
-                getword=in.readLine();
-                System.out.println("Input word is "+ getword);
+                getword = in.readLine();
+                System.out.println("Input word is " + getword);
+
                 for (int i = 0; i < 5; i++) {
                     randch += alphabet.charAt(r.nextInt(52));
                 }
+
                 SimpleDateFormat stime = new SimpleDateFormat("dd/MM/yy hh:mm:ss");
 
                 for (int i = 0; i < getword.length(); i++) {
                     char ch = getword.charAt(i);
                     hexword += String.format("%02X ", (int) ch);
                 }
-                result=randch+" "+stime.format(d)+" "+hexword;
-                System.out.println(result);
 
-                out.writeUTF(" "+result+" ");
+                result = randch + " " + stime.format(d) + " " + hexword;
+                System.out.println(result);
+                out.writeUTF(" " + result + " ");
                 out.flush();
 
-                if (getword.equals("q") || getword.equals("quit")) {
+                if (getword.equals("q") || getword.equals("quit") || getword.equals("exit")) {
                     break;
                 }
             }
-        } catch(Exception e) { e.printStackTrace(); }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
